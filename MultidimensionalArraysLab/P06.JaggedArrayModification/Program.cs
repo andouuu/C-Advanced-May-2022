dca5ed7c6a61b989146198114c098c8871f4a1e0
Andrian Dimitrov <andihacera@abv.bg>
2022-05-24 20:38:05 +0300

    Multidimensional Arrays Lab is done

--- MultidimensionalArraysLab/P06.JaggedArrayModification/Program.cs
+++ MultidimensionalArraysLab/P06.JaggedArrayModification/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
 
 namespace P06.JaggedArrayModification
 {
@@ -6,7 +8,46 @@ namespace P06.JaggedArrayModification
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jagArr= new int[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                jagArr[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+            string[] cmd = Console.ReadLine().Split();
+            while (cmd[0]!="END")
+            {
+                string cmdType = cmd[0];
+                
+                if (int.Parse(cmd[1])>=rows
+                    || int.Parse(cmd[1]) < 0
+                    || int.Parse(cmd[2]) < 0
+                    ||jagArr[int.Parse(cmd[1])].Length< int.Parse(cmd[2]))
+                {
+                    Console.WriteLine($"Invalid coordinates");
+                }
+                else if (cmdType=="Add")
+                {
+                    int currRow = int.Parse(cmd[1]);
+                    int currCol = int.Parse(cmd[2]);
+                    int currVal = int.Parse(cmd[3]);
+                    jagArr[currRow][currCol] += currVal;
+                }
+                else if (cmdType=="Subtract")
+                {
+                    int currRow = int.Parse(cmd[1]);
+                    int currCol = int.Parse(cmd[2]);
+                    int currVal = int.Parse(cmd[3]);
+                    jagArr[currRow][currCol]-=currVal;
+                }
+                cmd = Console.ReadLine().Split();
+            }
+
+            foreach (int[] arr in jagArr)
+            {
+                Console.WriteLine(string.Join(" ",arr));
+                
+            }
         }
     }
 }
--- MultidimensionalArraysLab/P07.PascalTriangle/Program.cs
+++ MultidimensionalArraysLab/P07.PascalTriangle/Program.cs
@@ -6,7 +6,26 @@ namespace P07.PascalTriangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            long[][] jagArr=new long[n][];
+            for (int i = 0; i < n; i++)
+            {
+                jagArr[i] = new long[i+1];
+                jagArr[i][0] = 1;
+                jagArr[i][i] = 1;
+                if (i>1)
+                {
+                    for (int j = 1; j < i; j++)
+                    {
+                        jagArr[i][j] = jagArr[i - 1][j - 1] + jagArr[i - 1][j];
+                    }
+                }
+            }
+
+            foreach (var arr in jagArr)
+            {
+                Console.WriteLine(string.Join(" ",arr));
+            }
         }
     }
 }
