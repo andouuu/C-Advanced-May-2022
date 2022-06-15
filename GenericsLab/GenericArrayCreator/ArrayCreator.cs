c81172dab8d60b9cbe98dd818097ed03240bd3ee
Andrian Dimitrov <andihacera@abv.bg>
2022-06-15 22:38:16 +0300

    Generics Lab done

---
+++ GenericsLab/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,15 @@
+﻿namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] array = new T[length];
+            for (var index = 0; index < array.Length; index++)
+            {
+                array[index] = item;
+            }
+            return array;
+        }
+    }
+}
