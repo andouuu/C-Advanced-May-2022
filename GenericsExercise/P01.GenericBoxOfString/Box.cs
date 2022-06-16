8d2d8ee38d5886f4095b22c288d3a9b9890f1590
Andrian Dimitrov <andihacera@abv.bg>
2022-06-16 15:19:12 +0300

    First 2 problems done

---
+++ GenericsExercise/P01.GenericBoxOfString/Box.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace P01.GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public List<T> data;
+
+        public Box()
+        {
+            data=new List<T>();
+        }
+
+        public override string ToString()
+        {
+            string output = string.Empty;
+            foreach (T item in data)
+            {
+                output += $"{item.GetType()}: {item}" + "\r\n";
+            }
+            return output;
+        }
+    }
+}
---
+++ GenericsExercise/P01.GenericBoxOfString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using P01.GenericBoxOfString;
+
+namespace P01P02.GenericBoxOfStringAndInt
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {   
+            //// P01
+            //Box<string> box = new Box<string>();
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < n; i++)
+            //{
+            //    box.data.Add(Console.ReadLine());
+            //}
+            //Console.WriteLine(box.ToString());
+
+
+            ////P02
+            Box<int> intBox = new Box<int>();
+            int m = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++)
+            {
+                intBox.data.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(intBox.ToString());
+        }
+    }
+}
