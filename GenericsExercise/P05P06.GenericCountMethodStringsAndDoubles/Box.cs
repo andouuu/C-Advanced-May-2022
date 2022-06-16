1d86942c4ed4074cdbae6d314feac1ae83c201fe
Andrian Dimitrov <andihacera@abv.bg>
2022-06-16 16:09:32 +0300

    P05 done

---
+++ GenericsExercise/P05P06.GenericCountMethodStringsAndDoubles/Box.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05P06.GenericCountMethodStringsAndDoubles
+{
+    public class Box<T>:IComparable<T> where T : IComparable<T>
+    {
+        public List<T> data;
+        public T Element;
+        public Box(T element)
+        {
+            Element=element;
+        }
+        public Box(List<T> data)
+        {
+            this.data=data;
+        }
+        public void SwapPlaces(int index1, int index2)
+        {
+
+            T elementIndex1=data[index1];
+            data[index1]=data[index2];
+            data[index2]=elementIndex1;
+        }
+
+        public int CompareTo(T other) => Element.CompareTo(other);
+
+        public int CountGreaterElements<T>(List<T> list, T readline) where T : IComparable =>
+            list.Count(x => x.CompareTo(readline) > 0);
+
+        public override string ToString()
+        {
+            string output = string.Empty;
+            foreach (T item in data)
+            {
+                output += $"{item.GetType()}: {item}" + "\r\n";
+            }
+            return output;
+        }
+
+        
+    }
+}
---
+++ GenericsExercise/P05P06.GenericCountMethodStringsAndDoubles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05P06.GenericCountMethodStringsAndDoubles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //P05
+            int n = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            Box<string> stringBox = new Box<string>(list);
+            string elToCompare = Console.ReadLine();
+            int count = stringBox.CountGreaterElements(stringBox.data, elToCompare);
+            Console.WriteLine(count);
+        }
+        
+        
+    }
+}
