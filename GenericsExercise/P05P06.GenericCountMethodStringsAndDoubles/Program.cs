05e7931d791fd41c94df971cdbc6089d13e2b34e
Andrian Dimitrov <andihacera@abv.bg>
2022-06-16 16:11:36 +0300

    P06 done

--- GenericsExercise/P05P06.GenericCountMethodStringsAndDoubles/Program.cs
+++ GenericsExercise/P05P06.GenericCountMethodStringsAndDoubles/Program.cs
@@ -8,18 +8,31 @@ namespace P05P06.GenericCountMethodStringsAndDoubles
         static void Main(string[] args)
         {
             //P05
+            //int n = int.Parse(Console.ReadLine());
+            //List<string> list = new List<string>();
+            //for (int i = 0; i < n; i++)
+            //{
+            //    list.Add(Console.ReadLine());
+            //}
+            //Box<string> stringBox = new Box<string>(list);
+            //string elToCompare = Console.ReadLine();
+            //int count = stringBox.CountGreaterElements(stringBox.data, elToCompare);
+            //Console.WriteLine(count);
+
+
+
+
+            //P06
             int n = int.Parse(Console.ReadLine());
-            List<string> list = new List<string>();
+            List<double> list = new List<double>();
             for (int i = 0; i < n; i++)
             {
-                list.Add(Console.ReadLine());
+                list.Add(double.Parse(Console.ReadLine()));
             }
-            Box<string> stringBox = new Box<string>(list);
-            string elToCompare = Console.ReadLine();
+            Box<double> stringBox = new Box<double>(list);
+            double elToCompare = double.Parse(Console.ReadLine());
             int count = stringBox.CountGreaterElements(stringBox.data, elToCompare);
             Console.WriteLine(count);
         }
-        
-        
     }
 }
