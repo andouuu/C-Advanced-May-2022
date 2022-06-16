7d6f9b220df5e6c996ebcb5e603eb9745e38fdc3
Andrian Dimitrov <andihacera@abv.bg>
2022-06-16 15:32:36 +0300

    P04 Done

--- GenericsExercise/P03P04.GenericSwapMethodStringAndInteger/Program.cs
+++ GenericsExercise/P03P04.GenericSwapMethodStringAndInteger/Program.cs
@@ -7,19 +7,34 @@ namespace P03P04.GenericSwapMethodStringAndInteger
     {
         static void Main(string[] args)
         {
-            //P03
-            Box<string> stringBox=new Box<string>();
+            ////P03
+            //Box<string> stringBox=new Box<string>();
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < n; i++)
+            //{
+            //    stringBox.data.Add(Console.ReadLine());
+            //}
+
+            //int[] swapIndexes = Console.ReadLine()
+            //    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(int.Parse).ToArray();
+            //stringBox.SwapPlaces(swapIndexes[0], swapIndexes[1]);
+            //Console.WriteLine(stringBox.ToString());
+
+
+            ////P04
+            Box<int> intBox=new Box<int>();
             int n = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
-                stringBox.data.Add(Console.ReadLine());
+                intBox.data.Add(int.Parse(Console.ReadLine()));
             }
 
             int[] swapIndexes = Console.ReadLine()
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse).ToArray();
-            stringBox.SwapPlaces(swapIndexes[0], swapIndexes[1]);
-            Console.WriteLine(stringBox.ToString());
+            intBox.SwapPlaces(swapIndexes[0], swapIndexes[1]);
+            Console.WriteLine(intBox.ToString());
         }
     }
 }
