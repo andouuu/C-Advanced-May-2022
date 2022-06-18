81e5e79129f609646e2165af891210db7b71f93a
Andrian Dimitrov <andihacera@abv.bg>
2022-06-18 14:31:55 +0300

    Iterators and Comparators Lab is done

---
+++ IteratorsAndComparators/P01.Library/Library.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIndex=-1;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public bool MoveNext()
+            {
+                currentIndex++;
+                return this.currentIndex < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+
+            public Book Current => this.books[currentIndex];
+            object IEnumerator.Current => this.Current;
+            public void Dispose() { }
+        }
+
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            Array.Sort(books,new BookComparator());
+            this.books = books.ToList();
+        }
+    }
+}
---
+++ тест/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int i = int.Parse(Console.ReadLine());
+            int j = int.Parse(Console.ReadLine());
+            for (int k = i; k <= j; k++)
+            {
+                int iSwap = arr[k];
+                arr[k] = arr[j];
+                arr[j] = iSwap;
+                j--;
+            }
+
+            Console.WriteLine(string.Join(" ",arr));
+        }
+    }
+}
