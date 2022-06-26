04ae37277f0b823cc5c64bd1528b20efc06146eb
Andrian Dimitrov <andihacera@abv.bg>
2022-06-26 18:55:00 +0300

    testing some things

--- тест/ConsoleApp1/Program.cs
+++ тест/ConsoleApp1/Program.cs
@@ -9,30 +9,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
-            List<string> durjavi = Console.ReadLine().Split(",").ToList();
-            Console.WriteLine(string.Join(" ",durjavi));
-            int broi=0;
-            for (int i = 0; i < durjavi.Count; i++)
+            //Deklarirame opashkata
+            Queue<int> queue = new Queue<int>();
+            //deklarirame i klasut random
+            Random random = new Random();
+            //vzimame ot konzolata chislo N
+            int n=int.Parse(Console.ReadLine());
+            //pravim for cikul, koito da se vurti N puti
+            for (int i = 0; i < n; i++)
             {
-                if (durjavi[i][0]=='A')
+                //vkarvame v opashkata sluchaino chislo ot intervala [10,300]
+                queue.Enqueue(random.Next(10,301));
+                //vtoroto e 301 zashtoto poslednoto ne se broi toest se broqt samo chislata do 300
+            }
+            //izvejdame opashkata
+            Console.WriteLine(string.Join(" ",queue));
+            //deklarirame chetna i nechetna suma
+            int evenSum = 0;
+            int oddSum = 0;
+            //preminavame prez vseki element i go proverqvame dali e chetno ili neshetno
+            foreach (int i in queue)
+            {
+                if (i%2==0)
                 {
-                    durjavi.Remove(durjavi[i]);
-                    broi++;
-                    i = -1;
+                    evenSum += i;
+                }
+                else
+                {
+                    oddSum+=i;
                 }
             }
-
-            if (broi==0)
-            {
-                Console.WriteLine("There are not a country beginning with 'A' in the list.");
-            }
-            else
-            {
-                Console.WriteLine(broi);
-            }
-
-            Console.WriteLine(string.Join(";",durjavi));
+            //izpisvame proizvedenieto na dvete sumi
+            Console.WriteLine(evenSum*oddSum);
         }
     }
 }
